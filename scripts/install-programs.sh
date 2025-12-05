2612a0431b14f0153ff82ecc853a0b5b52e10217
natebru <1214808+natebru@users.noreply.github.com>
2025-12-05 10:28:53 -0800

    made some updates to reflect

--- scripts/install-programs.sh
+++ scripts/install-programs.sh
@@ -9,10 +9,20 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 ## Install Powerline fonts
 sudo apt install fonts-powerline
 
+
+## more fonts
+sudo wget -P /usr/local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+sudo wget -P /usr/local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+sudo wget -P /usr/local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+sudo wget -P /usr/local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+fc-cache -fv
+
 ## Install customizations
 cd ~/.oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting
+
 
 ## Back to Home
 cd
